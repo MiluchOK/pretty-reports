0f043ba14edbf6c800c7c9a92c89eaef4590462f
alexey <amilyukov@amilyukov.local>
2017-08-08 23:43:39 -0700

    Something working

--- lib/pretty_reports/custom_html_printer.rb
+++ lib/pretty_reports/custom_html_printer.rb
@@ -7,67 +7,47 @@ class CustomHtmlPrinter
     @output = output
     @tests = []
     @template = Nokogiri::HTML(File.open(File.open(File.join(File.dirname(__FILE__), '/templates/template_page.html'))))
-    @data_flow = @template.css('script')[-2]
   end
 
   def print_html_start(notification)
     puts "STARTING"
     file_path = File.join(File.dirname(__FILE__), '/templates/test.js')
     ugly_js = Closure::Compiler.new.compile(File.open(file_path, 'r'))
-    notification.count.times do
-      @tests << {}
-    end
-    cardsJson = @tests.to_json
-    set_test_cards(cardsJson)
     write_html_to_output
   end
 
   def pass_next_in_queue(passed)
     puts "PASSED"
-    test = @tests.detect { |test| test.empty? }
-    test.merge!({
+    @tests << {
                     status: 'passed',
                     title: passed.example.description,
                     description: passed.example.description
-                })
-    set_test_cards(@tests)
+                }
     write_html_to_output
   end
 
   def pending_next_in_queue(pending)
     puts "PENDING"
-    test = @tests.detect { |test| test.empty? }
-    test.merge!({
+    @tests << {
                     status: 'pending',
                     title: pending.example.description,
                     description: pending.example.description
-                })
-    set_test_cards(@tests)
+                }
     write_html_to_output
   end
 
   def fail_next_in_queue(failure)
     puts "DIDN'T PASS"
-    test = @tests.detect { |test| test.empty? }
-    test.merge!({
+    @tests << {
         status: 'failed',
         title: failure.example.description,
         description: failure.example.execution_result.exception.to_s
-    })
-    set_test_cards(@tests)
+    }
     write_html_to_output
   end
 
-  def flush
-    @output.flush
-  end
-
-  private
-  def set_test_cards(list_of_tests)
-    @data_flow.content = "var cards=#{list_of_tests.to_json}"
-  end
-
   def write_html_to_output
+    @template.css('script')[-2].content = "var cards=#{@tests.to_json}"
     @output.truncate 0
     @output.puts(@template.to_html)
   end
